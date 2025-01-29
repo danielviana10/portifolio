f6594594df8ed623eafba283caa662d3e392ff1e
danielviana10 <danielvianna43@gmail.com>
2025-01-29 03:08:11 -0300

    portifólio: início

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,14 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{html,js,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'navbar-bg': '#30333A',
+        'greenCustom': '#07E78F',
+        'greyCostum': 'var(--background)'
       },
     },
   },
